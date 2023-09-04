1a4cbb65b3ea7b290d2a37ed554853c51148d123
Gemechisgk <gemechis.gk@gamail.com>
2023-09-04 15:47:44 +0300

    conflict fix

--- lib/constants.dart
+++ lib/constants.dart
@@ -30,3 +30,8 @@ const Duration kRippleAnimationDuration = Duration(milliseconds: 400);
 const Duration kLoginAnimationDuration = Duration(milliseconds: 1500);
 
 
+// API KEY 
+class Constants {
+  static const apikey = '40829cee9c82218cc2d4c95a4ef7d994';
+  static const imageUrl = 'https://image.tmdb.org/t/p/w500';
+}
---
+++ lib/widgets/books_slider.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+import '../constants.dart';
+import '../screens/details_screens.dart';
+
+class BooksSlider extends StatelessWidget {
+  const BooksSlider({
+    super.key,
+    required this.snapshot,
+  });
+  final AsyncSnapshot snapshot;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      width: double.infinity,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        physics: const BouncingScrollPhysics(),
+        itemCount: snapshot.data!.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => DetailScreen(
+                      movie: snapshot.data[index], // Removed 'const' from here
+                    ),
+                  ),
+                );
+              },
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(8),
+                child: SizedBox(
+                  height: 200,
+                  width: 150,
+                  child: Image.network(
+                    filterQuality: FilterQuality.high,
+                    fit: BoxFit.cover,
+                    '${Constants.imageUrl}${snapshot.data![index].posterPath}',
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
